66f8feeb31fb7b1b114cea74a36f3a9b936e4977
Alexandru Frunza <alex@alexfrunza.com>
2024-06-20 20:38:30 +0300

    Add cors for 127.0.0.1 && fix register case items

--- actors-backend/routers/index.js
+++ actors-backend/routers/index.js
@@ -12,7 +12,7 @@ const actors = [
 export const mainRouter = async (req, res) => {
     try {
         // CORS headers
-        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+        res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:3000');
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
         res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-access-token');
 
