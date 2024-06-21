4e07f66ad5daff1a19d499eef73abeb2c20395b4
Stefan Vlad <72700839+StefanVlad0@users.noreply.github.com>
2024-06-21 14:13:38 +0300

    Get data in edit profile page

--- auth-backend/routers/users.js
+++ auth-backend/routers/users.js
@@ -1,4 +1,5 @@
 import {createUser} from "../controllers/users.js";
+import {getUser} from "../controllers/getUser.js";
 
 export const usersRouter = async (req, res) => {
     req.handled = true;
@@ -6,9 +7,9 @@ export const usersRouter = async (req, res) => {
     if (req.fullUrl.pathname === '/users' && req.method === 'GET') {
         req.handled = false;
         //
-    } else if (req.fullUrl.pathname.startsWith('/users/') && req.method === 'GET') {
-        req.handled = false;
-        // a single user
+    } else if (req.fullUrl.pathname.startsWith('/') && req.method === 'GET') {
+        console.log("GET /users/id");
+        await getUser(req, res);
     } else if (req.fullUrl.pathname === '/' && req.method === 'POST') {
         console.log("POST /users");
         await createUser(req, res);
