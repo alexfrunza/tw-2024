8c764b928e732ad76e8a97192a4bba80eb4cc572
Alexandru Frunza <alex@alexfrunza.com>
2024-06-22 18:38:40 +0300

    Fix links from navbar

--- frontend/js/components/navbar.js
+++ frontend/js/components/navbar.js
@@ -3,15 +3,15 @@ const navBarTemplate = document.createElement('template')
 navBarTemplate.innerHTML = `
 <nav class="navbar">
         <div class="navbar--logo">
-            <a href="index.html">AcVis</a>
+            <a href="/index.html">AcVis</a>
         </div>
         <div class="navbar__links" id="navbarLinks">
-            <a href="about.html">About</a>
-            <a href="actors.html">Actors</a>
-            <a href="actor.html">Actor</a>
-            <a href="edit_profile.html">Edit Profile</a>
-            <a href="register.html">Register</a>
-            <a href="login.html">Login</a>
+            <a href="/about.html">About</a>
+            <a href="/actors.html">Actors</a>
+            <a href="/actor.html">Actor</a>
+            <a href="/edit_profile.html">Edit Profile</a>
+            <a href="/register.html">Register</a>
+            <a href="/login.html">Login</a>
             <a href="#" id="logout">Logout</a>
         </div>
         <button class="navbar--toggle" id="navbarToggle">☰</button>
