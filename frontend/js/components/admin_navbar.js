e69a42c1477238d17253b44ac31993e60105bd7d
Alexandru Frunza <alex@alexfrunza.com>
2024-06-23 22:59:20 +0300

    Add award crud in admin dash

--- frontend/js/components/admin_navbar.js
+++ frontend/js/components/admin_navbar.js
@@ -2,16 +2,15 @@ const adminNavbarTemplate = document.createElement('template')
 
 adminNavbarTemplate.innerHTML = `
 <div class="users-panel__menu">
-    <button class="users-panel__menu--button">Add User</button>
-    <button class="users-panel__menu--button">Delete Users</button>
-    <button class="users-panel__menu--button">Export Users</button>
-
     <a href="/admin_panel/add_show.html" class="users-panel__menu--button">Add Show</a>
     <a href="/admin_panel/shows.html" class="users-panel__menu--button">See Shows</a>
 
     <a href="/admin_panel/add_actor.html" class="users-panel__menu--button">Add Actor</a>
     <a href="/admin_panel/actors.html" class="users-panel__menu--button">See Actors</a>
 
+    <a href="/admin_panel/add_award.html" class="users-panel__menu--button">Add Award</a>
+    <a href="/admin_panel/awards.html" class="users-panel__menu--button">See Awards</a>
+    
     <a href="/admin_panel/import_actors.html" class="users-panel__menu--button">Import actors db</a>
     <a href="/admin_panel/export_actors.html" class="users-panel__menu--button">Export actors db</a>
 </div>
